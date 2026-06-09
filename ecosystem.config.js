ea31afda1774d5d5c7476ef6bb5dc60dda24fc7d
boripat874 <boripat874@gmail.com>
2026-06-09 11:38:36 +0700

    ver1.0.2

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: "Kiosk-Server",
+    script: 'server.js',
+    instances  : 'max',
+    watch: true,
+    env: {
+      NameDatabase: "pg",
+      Host: "10.10.17.4",
+      Users: "postgres",
+      Password: "zoo@kiosk2026_pgsql",
+      Database: "kiosk",
+      Port: 443
+    },
+  }],
+};
